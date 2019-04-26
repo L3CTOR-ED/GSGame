5e6957e6a678689df9d9a135ccdc23e3b7f7e075
reworr <kerim-rew@mail.ru>
2019-04-26 15:07:23 +0300

    Add files via upload

--- auth/index.php
+++ auth/index.php
@@ -12,23 +12,58 @@ if($_SESSION["name"]){
     ?><img src="https://cs.pikabu.ru/images/big_size_comm/2013-02_1/1359705436774.jpeg"><br><br>
     <font size="6"><a href="/">Вернуться</a></font><?php die();
 } ?>
-<!DOCTYPE html>
+  
 <html>
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../style.css">
+    <title>GSGame</title>
 </head>
 <body>
-<h2>Авторизация</h2>
-    <form method="POST" action="auth.php">
-        Имя: <input name="name" type="text"><br>
-        Password: <input name="password" type="password"><br>
-        <input type="submit" value="Отправить">
-    </form><br>
-<font size="5"><a href="/">Вернуться</a></font>
+    <header>
+        <img src="../imgs/logo.png" id="logo">
+        <p id="logoText">GSGame</p>
+        <p id="social_media"><a href="https://vk.com">Вк</a>/<a href="https://instagram.com">Инстаграм</a></p>
+        <a href="auth/index.php" id="auth">Войти</a>
+    </header>
+    <div id="registration">
+        <div id="regform">
+            <h1>Войти в учётную запись</h1><br>
+            <form  method="POST" action="auth.php">
+                <span>электронная почта</span><br><input class="field" type="text" name="name"><br>
+                <span>пароль</span><br><input class="field" type="password" name="password"><br>
+                <p><input type="submit" id="buttonreg" value="Продолжить"></p>
+            </form>
+        </div>
+    </div>
+    <div id="about_us">
+        <p id="authors">Разработчики</p>
+        <div class="author">
+            <img src="../imgs/author1.png">
+            <p>Автор Авторов</p>
+        </div>
+        <div class="author">
+            <img src="../imgs/author2.png">
+            <p>Автор Авторов</p>
+        </div>
+        <div class="author">
+            <img src="../imgs/author3.png">
+            <p>Автор Авторов</p>
+        </div>
+        <div class="author">
+            <img src="../imgs/author4.png">
+            <p>Автор Авторов</p>
+        </div>
+        <div class="author">
+            <img src="../imgs/author5.png">
+            <p>Автор Авторов</p>
+        </div>
+    </div>
+    <footer>
+        <a href="#">Ссылки</a>
+        <a href="#">О нас</a>
+    </footer>
 </body>
 </html>
