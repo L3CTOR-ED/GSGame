8e1b1b3f2fdc6bc4a18886d77accd1ba893c2409
reworr <kerim-rew@mail.ru>
2019-04-26 15:40:47 +0300

    Add files via upload
    рефакторинг

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ echo("<h2>Hello, " . $_SESSION['name']); }
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Kavo?</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,5 +16,6 @@ echo("<h2>Hello, " . $_SESSION['name']); }
     <h3><a href="/reg/" style="color: rgb(255, 72, 0)">Регистрация</a></h3>
     <h3><a href="/admin/" style="color: rgb(255, 72, 0)">Вход в админку</a></h3>
     <h3><a href="/admin/admin.php" style="color: rgb(255, 72, 0)">Сама админка (для админов)</a></h3>
+    <h3><a href="/users" style="color: rgb(255, 72, 0)">Пользователи</a></h3>
 </body>
 </html>
